768e2c39d4abdaf8d6219fa112a39103b24bebfc
aidishage <aidishage@gmail.com>
2021-02-17 16:47:34 +0800

    master

--- test.py
+++ test.py
@@ -11,3 +11,11 @@ print(torch.cuda.get_device_name(0))
 print(torch.rand(3, 3).cuda())
 
 # test
+#%%
+import tensorflow as tf
+
+with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as sess:
+    a = tf.constant(1)
+    b = tf.constant(3)
+    c = a + b
+    print('结果是：%d\n值为：%d' % (sess.run(c),sess.run(c)))
