78802a0c9a18786ed61d3d618573636a668dff39
Song921012 <aidishage@gmail.com>
2021-02-24 09:26:56 +0800

    Create README.md

--- test.py
+++ test.py
@@ -19,3 +19,8 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
     b = tf.constant(3)
     c = a + b
     print('结果是：%d\n值为：%d' % (sess.run(c),sess.run(c)))
+
+#%%
+import numpy as np
+print(np.exp(0))
+# %%
